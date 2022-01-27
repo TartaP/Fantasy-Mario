1fe4bdc91254d384b61b82fb7a7ea5deac9ae09b
TartaP <zeng4129@gmail.com>
2022-01-26 23:19:58 -0500

    Enemy Sprite/NewScene
    raninto an error so i remade the scene but kept the assets

--- Project 1/Assets/Scripts/LivesCountdown.cs
+++ Project 1/Assets/Scripts/LivesCountdown.cs
@@ -25,7 +25,7 @@ public class LivesCountdown : MonoBehaviour
 
         if (timer > 4)
         {
-            SceneManager.LoadScene("GameScene"); 
+            SceneManager.LoadScene("FirstLevel"); 
         }
 
     }
